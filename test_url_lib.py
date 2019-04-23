6f62c3afda1bf9df51dd4c1b66d9558650344a2a
Ratatouille <307338947@qq.com>
2019-04-23 19:54:45 +0800

    测试requests包

--- test_url_lib.py
+++ test_url_lib.py
@@ -37,22 +37,73 @@ import json
 
 # 3.test headers
 # request the web site without header is likely to be refused, like:
-response = requests.get("http://www.zhihu.com")
-print(response.text)
+# response = requests.get("http://www.zhihu.com")
+# print(response.text)
+#
+# """ without header, you'll get:
+# <html>
+# <head><title>400 Bad Request</title></head>
+# <body bgcolor="white">
+# <center><h1>400 Bad Request</h1></center>
+# <hr><center>openresty</center>
+# </body>
+# </html>
+# """
+# # test with header:
+# header = {
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+# }
+#
+# response_with_header = requests.get("http://www.zhihu.com", headers=header)
+# with open("./zhihu.html", "w", encoding="utf-8") as f_zhihu_html:
+#     f_zhihu_html.write(response_with_header.text)
+#
+# print(response_with_header)
 
-""" without header, you'll get:
-<html>
-<head><title>400 Bad Request</title></head>
-<body bgcolor="white">
-<center><h1>400 Bad Request</h1></center>
-<hr><center>openresty</center>
-</body>
-</html>
-"""
-# test with header:
-header = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
-}
+# 下载一张图片：
+# response = requests.get("https://www.baidu.com/img/xinshouye_c9d9de2ff40fa160f807f75f34db4ad0.gif")
+# with open(".\google_icon.gif", "wb") as google_icon:
+#     google_icon.write(response.content)
+# google_icon.close()
+# 上传图片：
+# with open(".\google_icon.gif", "rb") as google_icon_:
+#     files = {'file': google_icon_}
+#     response = requests.post("http://httpbin.org/post", files=files)
+# print("----->" + response.text)
 
-response_with_header = requests.get("http://www.zhihu.com", headers=header).content
-print(response_with_header)
+# -------------------------------------------------------------------------------------------
+# 获取cookie:
+# header = {
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+# }
+# response_get_cookie = requests.get("http://www.baidu.com", headers=header)
+# print(response_get_cookie.cookies)
+# print("-" * 200)
+# for key, value in response_get_cookie.cookies.items():
+#     print(key + "<--->" + value)
+
+# requests.get("http://httpbin.org/cookies/set/number/987654321")
+# response_set_and_get_cookies = requests.get("http://httpbin.org/cookies")
+# print(response_set_and_get_cookies.text)
+
+# # 保持对话： Session()类：
+# session_instance = requests.Session()
+# session_instance.get("http://httpbin.org/cookies/set/number/987654321")
+# response = session_instance.get("http://httpbin.org/cookies")
+# print(response.text)
+
+# # 证书验证：
+# response_verify = requests.get("http://www.12306.com")
+# print("--------------")
+# print(response_verify.status_code)
+# print("--------------")
+
+# # 设置代理：
+# proxies = {
+#     "http": "http://:9743",
+#     "https": "https://127.0.0.1:9743",
+# }
+# response_proxy = requests.get("https://www.taobao.com", proxies=proxies)
+# print(response_proxy)
+
+
